94041bbbf905961a0dc4f99dd3d6cd251f0f431a
Shahzada Ahsan Raza <52956398+Ahsan-Raza42@users.noreply.github.com>
2022-06-20 14:57:05 +0500

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = '${1:url}';
+    public $baseURL = '';
 
     /**
      * --------------------------------------------------------------------------
